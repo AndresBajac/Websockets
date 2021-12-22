111539ab88adedf16adb3a19bde8706604eb1a2b
Andrés Bajac <83318943+AndresBajac@users.noreply.github.com>
2021-12-22 12:21:16 -0300

    función de validar email mediante regEx

--- public/index.js
+++ public/index.js
@@ -69,3 +69,14 @@ $("#chatForm").submit(e => {
     
     socket.emit("newMessage", message);
 })
+
+function validarEmail(valor) {
+    let valid = false;
+    //console.log(valor)
+    if (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(valor)){
+      valid = true;
+    }
+    //console.log(valid)
+    return valid;
+    
+  }
